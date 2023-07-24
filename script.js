b03fa815dbfbd7ed3930cfe85386fd587664a2e9
Govind Bajaj <meetgovindbajaj@gmail.com>
2023-07-24 22:59:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.body.onload = createBoxes();
+
+function createBoxes() {
+  const container = document.getElementById("container");
+  for (let i = 1; i <= 800; i++) {
+    const box = document.createElement("div");
+    const id = `box${i}`;
+    box.className = "square";
+    box.id = id;
+    box.onmouseenter = () => handleHoverEnter(id);
+    box.onmouseleave = () => handleHoverLeave(id);
+    container.appendChild(box);
+  }
+}
+
+function handleHoverEnter(id) {
+  const box = document.getElementById(id);
+  box.style.background = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(
+    Math.random() * 255
+  )}, ${Math.floor(Math.random() * 255)})`;
+}
+function handleHoverLeave(id) {
+  const box = document.getElementById(id);
+  setTimeout(() => {
+    box.style.backgroundColor = "rgb(36, 36, 36)";
+  }, 1000);
+}
